558042a8db0d2fefa2aaa0b8bdfe64606b067434
JakeHuneau <jakehuneau@yahoo.com>
2021-12-01 18:40:05 -0500

    Simplified and added docs

--- day1/day1.py
+++ day1/day1.py
@@ -1,32 +1,22 @@
-def part_1():
-    prev_num = None
-    tot_increases = 0
-
-    # part 1
-    for num in open('day1/data', 'r'):
-        if prev_num:
-            if int(num) > prev_num:
-                tot_increases += 1
-        prev_num = int(num)
-    
-    print(f'part 1: {tot_increases}')
-
-def part_2():
-    window_size = 3
-    window = [None] * window_size
-    slider = 0
+def windowed_comparison(window_size: int):
+    """
+    When comparing the sum of a window vs the next window, we only need to look at the
+    first value of the window vs the new value we're encountering.
+    """
+    window = [None] * window_size  # Start with None so we can start by building window
+    slider = 0  # Where in the window we're at
 
     tot_increases = 0
 
     for num in open('day1/data', 'r'):
-        if window[window_size - 1]:
-            if int(num) > window[slider]:
+        if window[window_size - 1]:  # Skip first <window_size> values to build initial window
+            if int(num) > window[slider]: 
                 tot_increases += 1
         window[slider] = int(num)
-        slider = (slider + 1) % window_size
+        slider = (slider + 1) % window_size  # Move slider up and wrap around
 
-    print(f'part 2: {tot_increases}')
+    return tot_increases
 
 if __name__ == '__main__':
-    part_1()
-    part_2()
+    print(f'part 1: {windowed_comparison(1)}')
+    print(f'part 2: {windowed_comparison(3)}')
