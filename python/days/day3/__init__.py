36d238d20732ab7b87bd44c5125247ef1339fbdd
JakeHuneau <jakehuneau@yahoo.com>
2021-12-03 09:19:02 -0500

    Simplified day2

--- python/days/day3/__init__.py
+++ python/days/day3/__init__.py
@@ -5,10 +5,18 @@ from types import EllipsisType
 def get_input():
     return [line.strip() for line in open("../input/day3", "r")]
 
-def binary_swap(data):
+def binary_flip(data):
+    """
+    Flips 1 -> 0, 0 -> 1 for all bits
+    """
     return ['0' if d == '1' else '1' for d in data]
 
 def get_gamma(data, equality):
+    """
+    Gets the gamma value by counting the 1's in each place and then 
+    comparing that to the total number of numbers to determine if 1 is
+    more common
+    """
     binary_count = None
     total_numbers = 0
     for num in data:
@@ -24,8 +32,11 @@ def get_gamma(data, equality):
     return ['1' if bit_count > total_numbers / 2 else '0' for bit_count in binary_count]
 
 def get_gamma_epsilon(data, equality=False):
+    """
+    epsilon is just a binary flip of gamma
+    """
     gamma = get_gamma(data, equality)
-    epsilon = binary_swap(gamma)
+    epsilon = binary_flip(gamma)
     return ''.join(gamma), ''.join(epsilon)
 
 def part_1(data):
@@ -33,6 +44,9 @@ def part_1(data):
     return int(gamma, 2) * int(epsilon, 2)
 
 def part_2(data):
+    """
+    Need to recalculate gamma and epsilon after each filtering
+    """
     o2_list = [d for d in data]
     o2_index = 0
 
@@ -40,27 +54,16 @@ def part_2(data):
     co2_index = 0
 
     while len(o2_list) > 1:
-        gamma, epsilon = get_gamma_epsilon(o2_list, True)
-        temp_o2_list = []
-        for o2 in o2_list:
-            if o2[o2_index] == gamma[o2_index]:
-                temp_o2_list.append(o2)
-        o2_list = temp_o2_list
+        gamma, _ = get_gamma_epsilon(o2_list, True)
+        o2_list = list(filter(lambda o2: o2[o2_index] == gamma[o2_index], o2_list))
         o2_index += 1
-    o2_value = o2_list[0]
 
     while len(co2_list) > 1:
-        gamma, epsilon = get_gamma_epsilon(co2_list, True)
-
-        temp_co2_list = []
-        for co2 in co2_list:
-            if co2[co2_index] == epsilon[co2_index]:
-                temp_co2_list.append(co2)
-        co2_list = temp_co2_list
+        _, epsilon = get_gamma_epsilon(co2_list, True)
+        co2_list = list(filter(lambda co2: co2[co2_index] == epsilon[co2_index], co2_list))
         co2_index += 1
-    co2_value = co2_list[0]
 
-    return int(o2_value, 2) * int(co2_value, 2)
+    return int(o2_list[0], 2) * int(co2_list[0], 2)
 
 def solve():
     data = get_input()
