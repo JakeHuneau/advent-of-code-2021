7f60fdb7ef0ae5b922106a2b49757c5bd7695baa
JakeHuneau <jakehuneau@yahoo.com>
2021-12-03 21:44:40 -0500

    day1 rust

---
+++ rust/src/days/day01.rs
@@ -0,0 +1,22 @@
+#![allow(unused)]
+use crate::{Solution, SolutionPair};
+use std::fs::read_to_string;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn solve() -> SolutionPair {
+    let numbers: Vec<u32> = read_to_string("../../input/day1")
+        .unwrap()
+        .lines()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let sol1 = get_sol(&numbers, 1);
+    let sol2 = get_sol(&numbers, 3);
+
+    (Solution::UInt(sol1), Solution::UInt(sol2))
+}
+
+fn get_sol(ls: &[u32], n: usize) -> u64 {
+    ls.windows(n + 1).filter(|x| x[n] > x[0]).count() as u64
+}
---
+++ rust/src/days/day13.rs
@@ -0,0 +1,8 @@
+#![allow(unused)]
+use crate::{Solution, SolutionPair};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn solve() -> SolutionPair {
+    todo!()
+}
